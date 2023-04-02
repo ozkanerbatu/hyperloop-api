8956a1b856cc2a401546510e6bbec38c0df10512
Batuhan Özkaner <ozkanerbatuhan@gmail.com>
2023-04-02 20:29:11 +0300

    emitting data and getting data from raspberry pi

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.emit("my_message", "Hello from server");
+  socket.on("my response", (msg) => {
+    console.log("message: " + JSON.stringify(msg));
+  });
 });
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("disconnect", (socket) => {
+  console.log("user disconnected", socket.id);
 });
-
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
