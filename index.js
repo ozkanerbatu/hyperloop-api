f01230086bbd53024f6280afa98f0a87b99e58a2
Batuhan Özkaner <ozkanerbatuhan@gmail.com>
2023-04-07 17:07:39 +0300

    update

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const io = new Server(server);
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
   socket.emit("my_message", "Hello from server");
-  socket.on("my response", (msg) => {
-    console.log("message: " + JSON.stringify(msg));
-  });
+  socket.emit("batter1",{id:1,value:0.5})
+  socket.emit("velocity",{id:1,value:{
+    x:0.5,
+    y:0.5,
+    z:0.5
+  }})
 });
+
 io.on("disconnect", (socket) => {
   console.log("user disconnected", socket.id);
 });
