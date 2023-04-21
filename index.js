4c5028cc6ce6f6a42395d478566f04de9202a07e
Batuhan Özkaner <ozkanerbatuhan@gmail.com>
2023-04-21 17:22:34 +0300

    update

--- index.js
+++ index.js
@@ -6,36 +6,58 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
-  socket.emit("levitasyonBatarya",80)
-  socket.on("_levitasyonSicakligi",(data)=>{
-    socket.emit("levitasyonSicakligi",data)
+  socket.emit("connected", socket.id);
+  socket.on("lbs",(data)=>{
+    sendData("levitasyonBatarya",data)
   })
-  socket.on("_ortamSıcaklığı",(data)=>{
+  socket.on("os",(data)=>{
+   console.log("ortam sıcaklığı",data);
     socket.emit("ortamSıcaklığı",data)
   })
-  socket.on("_itkiSıcaklığı",(data)=>{
+  socket.on("ims",(data)=>{
+   console.log("itki sıcaklığı",data);
     socket.emit("itkiSıcaklığı",data)
   })
-  socket.on("_levitasyonBatarya",(data)=>{
+  socket.on("b2",(data)=>{
+   console.log("levitasyon batarya",data);
     socket.emit("levitasyonBatarya",data)
   })
-  socket.on("_motorSürücüBatarya",(data)=>{
+  socket.on("b1",(data)=>{
+   socket.emit("motorSürücüBatarya",data)
     socket.emit("_motorSürücüBatarya",data)
   })
-  socket.on("_electronicBatarya",(data)=>{
+  socket.on("b3",(data)=>{
+    socket.emit("elektronikBatarya",data)
     socket.emit("electronicBatarya",data)
   })
-  socket.on("_itkiBatarya",(data)=>{
-    socket.emit("itkiBatarya",data)
+  socket.on("b4",(data)=>{
+    console.log("itki batarya",data);
+    socket.emit("ssss",data)
   })
-  
-  socket.emit("my_message", "Hello from server");
-  socket.emit("batter1",{id:1,value:0.5})
-  socket.emit("velocity",{id:1,value:{
-    x:0.5,
-    y:0.5,
-    z:0.5
-  }})
+  socket.on("ld",(data)=>{
+   // console.log("lidar",data);
+    socket.emit("lidar",data)
+  })
+  socket.on("kr",(data)=>{
+  //  socket.emit("renkSensoru",data)
+    socket.emit("renkSensoru",data)
+  })
+  socket.on("gx",(data)=>{
+   // console.log(data,"gx");
+    socket.emit("gx",data)
+  })
+  socket.on("gy",(data)=>{
+  //  console.log(data,"gy");
+    socket.emit("gy",data)
+  })
+  socket.on("gz",(data)=>{
+   // console.log(data,"gz");
+    socket.emit("gz",data)
+  })
+  socket.on("rpm",(data)=>{
+    console.log(data,"rpm");
+    socket.emit("hız",data)
+  })
 });
 
 io.on("disconnect", (socket) => {
@@ -44,3 +66,12 @@ io.on("disconnect", (socket) => {
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
+
+const sendData = (key,data) => {
+  try {
+    io.to(socket.id).emit(key,data)
+  }
+  catch (e) {
+    console.log(e);
+  }
+};
